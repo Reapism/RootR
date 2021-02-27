a46f16343a8e6a8e409179de50629de0b8fdc8e7
Anthony J <reapsprgm@gmail.com>
2021-02-26 21:28:33 -0500

    Add Random provider

---
+++ RootR.Shared/Services/RandomProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rootr.Core.Services
+{
+    public static class RandomProvider
+    {
+        public static Random Random { get; } = new Random();
+    }
+}
