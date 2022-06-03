2ef1e43e729ad83523b6fb1016f7be89800e9dea
Anthony J <reapsprgm@gmail.com>
2022-06-03 02:08:55 -0400

    Add new wpf project
    - initial layout 25% done

---
+++ Rootr.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rootr.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ProgressBar_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is ProgressBar p)
+            {
+                p.Value += e.ClickCount;
+            }
+        }
+    }
+}
